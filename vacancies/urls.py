5ea86b6e10eff53d6250e5f603f9e25e601809b9
Maria <marylevchenko@gmail.com>
2020-09-10 21:58:07 +0300

    add urls

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+    path('vacancies', views.list_vacancies, name='vacancies'),
+    path('vacancy/<int:vacancy>/', views.one_vacancy, name='vacancy'),
+    path('company', views.company, name='company'),
 	]
--- vacancies/views.py
+++ vacancies/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+def list_vacancies(request):
+    return render(request, 'vacancies.html', {})
+
+def vacancy_category(request, category):
+    return render(request, 'vacancies.html', {})
+
+def company(request, company):
+    return render(request, 'company.html', {})
+
+def one_vacancy(request, vacancy):
+    return render(request, 'vacancy.html', {})
