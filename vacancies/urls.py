fcea55aebfc18c0db5003d8a9f73ba6f76ef4421
Maria <marylevchenko@gmail.com>
2020-09-20 12:09:08 +0300

    prepare for deploy

--- vacancies/urls.py
+++ vacancies/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
+from django.urls import path
 
 from . import views
 
@@ -36,4 +36,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT)
+                          document_root=settings.MEDIA_ROOT)
